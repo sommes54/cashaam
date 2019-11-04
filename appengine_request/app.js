3f8978443511406ea76dcc853c1f47e869d3de22
Gbenga Awodokun <awodokoo@ucmail.uc.edu>
2019-11-04 15:43:03 -0600

    added twilio integration

--- appengine_request/app.js
+++ appengine_request/app.js
@@ -2,15 +2,17 @@
 
 // [START gae_node_request_example]
 const express = require("express");
+const dialogflow = require("dialogflow");
+const { Translate } = require("@google-cloud/translate").v2;
+const MessagingReponse = require("twilio").twiml.MessagingResponse;
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+const projectId = process.env.PROJECT_ID;
+
 const app = express();
-var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const dialogflow = require("dialogflow");
-const {Translate} = require('@google-cloud/translate').v2;
-const dotenv = require('dotenv');
-dotenv.config();
-const projectId = process.env.PROJECT_ID
 
 async function detectLanguage(text) {
   // Creates a client
@@ -18,7 +20,7 @@ async function detectLanguage(text) {
   let result = [];
   let [detections] = await translate.detect(text);
   detections = Array.isArray(detections) ? detections : [detections];
-  console.log('Detections:');
+  console.log("Detections:");
   detections.forEach(detection => {
     console.log(`${detection.input} => ${detection.language}`);
     result = detection.language;
@@ -33,14 +35,14 @@ async function detectLanguage(text) {
 async function runSample(msg) {
   // A unique identifier for the given session
   // const sessionId = uuid.v4();
-  console.log('message', msg);
+  console.log("message", msg);
   const sessionId = msg.From;
   // Create a new session
   const sessionClient = new dialogflow.SessionsClient();
   const sessionPath = sessionClient.sessionPath(projectId, sessionId);
   const langCode = await detectLanguage(msg.Body);
-  
-  console.log(langCode)
+
+  console.log(langCode);
   // The text query request.
   const request = {
     session: sessionPath,
@@ -69,16 +71,19 @@ async function runSample(msg) {
 
 app.post("/", (req, res) => {
   const message = req.body;
- 
-  runSample(message).then(result => {
-    res.status(200)
-    .send(result);
-    // .end();
-  }).catch(e =>{
-    console.log(e)
-    res.sendStatus(500)
-  })
-  
+  const twiml = new MessagingReponse();
+  runSample(message)
+    .then(result => {
+      twiml.message(result);
+      // res.status(200)
+      res.writeHead(200, { "Content-Type": "text/xml" })
+        // .send(twiml.toString());
+      res.end(twiml.toString());
+    })
+    .catch(e => {
+      console.log(e);
+      res.sendStatus(500);
+    });
 });
 
 // Start the server
